60edaaa17c01a7e2125c2cdca744d48cf76a3dea
Preet Dha <manpreetdha+github@gmail.com>
2017-04-20 13:43:29 -0700

    Add logic to vampire program

---
+++ ruby/vampires.rb
@@ -0,0 +1,55 @@
+current_year = 2017
+
+puts "How many employees will be processed in this survey?"
+num_of_employees = gets.to_i
+
+counter = 0
+
+while counter < num_of_employees  
+
+  puts "What is your name, employee number #{counter}?"
+  vampire_name = gets.chomp
+
+  puts "How old are you, #{vampire_name}?"
+  vampire_age = gets.to_i
+
+  puts "What year were you born?"
+  vampire_year = gets.to_i
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  garlic_bread = gets.chomp
+  if garlic_bread.downcase == "no"
+    garlic_bread = false 
+  else
+    garlic_bread = true 
+  end
+
+  puts "Would you like to enroll in the company’s health insurance?"
+  health_insurance = gets.chomp
+  if health_insurance.downcase == "no"
+    health_insurance = false 
+  else 
+    health_insurance = true 
+  end 
+
+  if vampire_name == ("Drake Cula" || "Tu Fang")
+    puts "Definitely a vampire."
+    
+  elsif current_year - vampire_year == vampire_age && (garlic_bread || health_insurance)
+    puts "Probably not a vampire."
+
+
+  elsif current_year - vampire_year != vampire_age && (!garlic_bread || !health_insurance)
+    puts "Probably a vampire."
+
+
+  elsif current_year - vampire_year != vampire_age && (garlic_bread && health_insurance)
+    puts "Almost certainly a vampire."
+
+  else 
+    puts "Results inconclusive."
+
+  end
+
+  counter += 1
+end
