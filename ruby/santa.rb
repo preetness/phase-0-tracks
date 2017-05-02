4fc2e78461d26982c3f54b51f7688c77af07545e
Preet Dha <manpreetdha+github@gmail.com>
2017-05-01 18:56:09 -0700

    Finish release two; add getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,6 +3,8 @@ class Santa
 def initialize(gender, ethnicity)
   @gender = gender
   @ethnicity = ethnicity
+  @age = 0
+  @reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
   puts "Initializing Santa instance..."
 end
 
@@ -16,18 +18,39 @@ def eat_milk_and_cookies(cookie_type)
 end
 
 def age
-  0
+  @age 
 end
 
-reindeer_ranking = ['Rudolph', 'Dasher', 'Dancer', 'Prancer', 'Vixen', 'Comet', 'Cupid', 'Donner', 'Blitzen']
+def ethnicity 
+  @ethnicity
+end
 
+def gender 
+  @gender
+end
+
+def reindeer_ranking
+  @reindeer_ranking
+end
+
+def celebrate_birthday
+  @age += 1
+end
+
+def gender=(change_gender)
+  @gender = change_gender
+end
+
+def get_mad_at(reindeer_name)
+  @reindeer_ranking.insert(-1, @reindeer_ranking.delete(reindeer_name))
+end
 
 end
 
 
-marcus = Santa.new
-marcus.speak
-marcus.eat_milk_and_cookies("chocolate chip")
+mr_claus = Santa.new('male', 'white')
+mr_claus.speak
+mr_claus.eat_milk_and_cookies("chocolate chip")
 
 santas = []
 
@@ -38,3 +61,13 @@ various_genders.length.times do |i|
   santas << Santa.new(various_genders[i], various_ethnicities[i])
 end
 
+p mr_claus.age
+p mr_claus.gender
+p mr_claus.ethnicity
+p mr_claus.reindeer_ranking
+p mr_claus.get_mad_at("Vixen")
+p mr_claus.reindeer_ranking
+mr_claus.celebrate_birthday
+p mr_claus.age
+mr_claus.gender = "all"
+p mr_claus.gender
