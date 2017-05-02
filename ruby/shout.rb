8788cdc889122f4b7edf9ad6809c211269007d52
Preet Dha <manpreetdha+github@gmail.com>
2017-05-02 10:57:59 -0700

    Add mixins to module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,41 @@
-module Shout
+# module Shout
   
-  def self.yell_angrily(words)
-    words + '!!!' + ' :('
+#   def self.yell_angrily(words)
+#     words + '!!!' + ' :('
+#   end
+
+#   def self.yelling_happily(words)
+#     'Yayyyy! ' + words + '!!!  :)'
+#   end
+
+# end
+
+module Shout
+
+  def yell_angrily(words)
+    puts words + '!!!' + ' :('
   end
 
-  def self.yelling_happily(words)
-    'Yayyyy! ' + words + '!!!  :)'
+  def yelling_happily(words)
+     puts 'Yayyyy! ' + words + '!!!  :)'
   end
 
 end
+
+class Coach
+  include Shout
+end
+
+
+class Mother
+  include Shout
+end
+
+
+coach = Coach.new
+coach.yell_angrily('What are you doing?')
+coach.yelling_happily('Great play!')
+
+mom = Mother.new
+mom.yell_angrily('Pick up your laundry!')
+mom.yelling_happily('I love you!')
