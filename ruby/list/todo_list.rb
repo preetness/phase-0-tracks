2acb8799d859ee553b45ffcd52aa4729c918952f
Preet Dha <manpreetdha+github@gmail.com>
2017-05-04 13:10:43 -0700

    Write methods to satisfy rspec tests for todo list

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,23 @@
+class TodoList
+
+def initialize(arr)
+  @arr = arr
+end
+
+def get_items
+  @arr
+end
+
+def add_item(item)
+  @arr << item
+end
+
+def delete_item(item)
+  @arr.delete(item)  
+end
+
+def get_item(index)
+  @arr[index]
+end
+
+end
