d1bbf5540da8baae6535ba0381b7783471d01e79
Preet Dha <manpreetdha+github@gmail.com>
2017-05-08 12:42:36 -0700

    Initial commit; write pseudocode to solve problem

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// Reverse a Word
+
+// We are going to write a function that takes in a word as an argument
+// Iterate through this word and store each letter into an array, backwards
+// Join the new array together into a string
+// Print the new string
+
