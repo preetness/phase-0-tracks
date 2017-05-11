faa63eeff006a90db820d6192ab0d45ebeff4511
Preet Dha <manpreetdha+github@gmail.com>
2017-05-11 11:04:17 -0700

    Rewrite method to encrypt a given name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,26 +2,26 @@
 
 #Change all of the vowels (a, e, i, or u) to the next vowel in 'aeiou' and all of the consonants to the next consonant
 
-def reverse_name(arr)
-  reversed_name = arr.split(' ').reverse.join(' ')
-  reversed_name
-end
+def spy_name(name)
 
-def next_vowel(char)
-  case char
-  when 'a'
-    char = 'e'
-  when 'e'
-    char = 'i'
-  when 'i'
-   char = 'o'
-  when 'o'
-   char = 'u'
-  when 'u'
-   char = 'a'
+new_name = name.downcase.split(' ').reverse.join(' ').split('')
+vowels = "aeiou".split("")
+consonants = "bcdfghjklmnpqrstvwxyz".split("")
+
+  spy = new_name.map do |letter|
+    if letter == "u"
+      letter = "a"
+    elsif letter == "z"
+      letter = "b"
+    elsif letter == ' '
+      letter = ' '
+    elsif index = vowels.index(letter)
+      letter = vowels[index+1]
+    elsif index = consonants.index(letter)
+      letter = consonants[index+1]
+    end
   end
-  p char
+  p spy.join('').split.map(&:capitalize).join(' ')
 end
 
-
-
+spy_name('Felicia Torres')
