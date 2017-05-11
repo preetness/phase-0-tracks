7ef9e11875a6bc8dffd2a59d76bc12b511e885cb
Preet Dha <manpreetdha+github@gmail.com>
2017-05-11 16:53:13 -0700

    Create a new constructor function, and create several new instances

--- js/data_structures.js
+++ js/data_structures.js
@@ -20,3 +20,31 @@ var horseColors = function(horse, color) {
 };
 
 horseColors(names, colors)
+
+// Create a new constructor function for a car, with properties and various data types (including a function)
+
+function Car(model, color, transmissionType, isFourDoor, cylinders) {
+
+  this.model = model;
+  this.color = color; 
+  this.transmissionType = transmissionType;
+  this.isFourDoor = isFourDoor;
+  this.cylinders = cylinders
+  this.honkHorn = function() { console.log('Honk! Honk!'); };
+
+};
+
+var carOne = new Car('Stargazer', 'red', 'manual', true, 6);
+console.log(carOne);
+carOne.honkHorn();
+console.log('The color of this ' + carOne.model + ' car is ' + carOne.color + '.');
+
+var carTwo = new Car('BeastMaster', 'black', 'automatic', false, 10);
+console.log(carTwo);
+carTwo.honkHorn();
+console.log('The ' + carTwo.model + ' car has ' + carTwo.cylinders + ' cylinders in it!');
+
+var carThree = new Car('Velor', 'white', 'automatic', true, 4);
+console.log(carThree);
+carThree.honkHorn();
+console.log('This brand new ' + carThree.model + ' is a white model with an ' + carThree.transmissionType + ' transmission in it.');
