dff696276122d98515081df9f3a51f1fcbc210ab
Preet Dha <manpreetdha+github@gmail.com>
2017-05-11 11:18:55 -0700

    Add a basic user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,6 +2,8 @@
 
 #Change all of the vowels (a, e, i, or u) to the next vowel in 'aeiou' and all of the consonants to the next consonant
 
+### BUSINESS LOGIC ###
+
 def spy_name(name)
 
 new_name = name.downcase.split(' ').reverse.join(' ').split('')
@@ -24,4 +26,15 @@ consonants = "bcdfghjklmnpqrstvwxyz".split("")
   p spy.join('').split.map(&:capitalize).join(' ')
 end
 
-spy_name('Felicia Torres')
+#spy_name('Felicia Torres')
+
+### USER INTERFACE ###
+
+loop do
+puts "Please enter a name to encrypt. Type 'quit' to exit."
+user_input = gets.chomp
+break if user_input == 'quit'
+spy_name = new_name(user_input)
+puts 'Your spy name encrypted is:'
+puts spy_name
+end
