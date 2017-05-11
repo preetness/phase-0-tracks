df950e977d9a65f23cd23f2ebd0c40483ce6b873
Preet Dha <manpreetdha+github@gmail.com>
2017-05-10 18:59:39 -0700

    Finish gps assignement by adding remove, update, and print methods

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -6,13 +6,12 @@
   # print the list to the console [can you use one of your other methods here?]
 # output: a hash of grocery list items
 
-def create_grocery_list(items)
-  grocery_list = {}
-  items.split(' ').each { |item| grocery_list[item] = 1 }
-  grocery_list
+def create_list(items)
+  list = {}
+  items.split(' ').each { |item| add_item(list, item) }
+  list
 end
 
-list = create_grocery_list("carrots apples cereal pizza")
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
@@ -21,22 +20,46 @@ list = create_grocery_list("carrots apples cereal pizza")
 
 def add_item(list, item, quantity = 1)
   list[item] = quantity
-  p list
+  list
 end
-
-add_item(list, "squash", 5)
 
 # Method to remove an item from the list
 # input:
 # steps:
 # output: An updated hash without the item removed
 
+def remove_from_list(list, item)
+  list.delete_if {|key, value| key == item}
+  list
+end
+
 # Method to update the quantity of an item
 # input:
 # steps:
 # output: An updated hash with the new quantity
 
+def update_quantity(list, item, quantity)
+  add_item(list, item, quantity)
+  list
+end
+
 # Method to print a list and make it look pretty
 # input:
 # steps:
 # output: A "prettier" grocery list
+
+def print_list(list)
+  list.each do |key, value| 
+    puts "#{key}: #{value}"
+  end
+end
+
+list = create_list("carrots apples cereal pizza")
+#print_list(list)
+add_item(list, 'squash', 5)
+add_item(list, 'oranges', 7)
+#print_list(list)
+remove_from_list(list, 'squash')
+update_quantity(list,'carrots', 10)
+print_list(list)
+
