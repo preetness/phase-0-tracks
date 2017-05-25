e25cef819308fc01124cebf49f2cead15070270a
Preet Dha <manpreetdha+github@gmail.com>
2017-05-24 22:05:50 -0700

    Add routes and erb file

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,7 +7,6 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
-# show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
   erb :home
@@ -17,11 +16,14 @@ get '/students/new' do
   erb :new_student
 end
 
-# create new students via
-# a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
 
-# add static resources
+get '/students' do
+  @students = db.execute("SELECT * FROM students") 
+  erb :students
+end
+
+
